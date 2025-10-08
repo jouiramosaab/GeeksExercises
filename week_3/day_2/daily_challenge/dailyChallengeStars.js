3055b8e2785a425a932bad2e9649dc3140d9d98e
jouiramosaab <mosaab.jouira.dev@gmail.com>
2025-10-08 15:13:13 +0100

    add

---
+++ week_3/day_2/daily_challenge/dailyChallengeStars.js
@@ -0,0 +1,19 @@
+// stars : 
+// methode 1 : 
+        
+        let stars = "*"
+        for(let i = 0 ; i < 6 ; i++){
+            console.log(stars)
+            stars +="*"
+        }
+
+        let star = "*"
+        for(let i = 0 ; i < 6 ; i++){
+            line = ""
+            for(e = 0 ; e<= i ; e++){
+                line += star + " "
+            }
+            console.log(line)
+        }
+
+
